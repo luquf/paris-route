463f2585f7e98356b0bc80a37e6c8a6bc759cc0a
Leo Berton <leoantoineberton@gmail.com>
2019-04-16 10:12:28 +0200

    Initial commit

---
+++ src/main/java/paris/route/App.java
@@ -0,0 +1,13 @@
+package paris.route;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
